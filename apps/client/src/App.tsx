cceb94206db2ff8768196c49cfb3471c706c41d9
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-05 23:24:50 +0200

    feat: established the client socket in order to test it
    Close: #8

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import io from 'socket.io-client';
+import './App.css';
+
+const socket = io('http://localhost:3000/test');
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  socket.emit('connection', 'hello');
+
+  socket.on('echo', (data) => {
+    console.log(data);
+  })
 
   return (
     <div className="App">
