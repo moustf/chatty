5d8648c8c68c99e6c841d5c605f7c89df90a6f90
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-05 23:22:27 +0200

    feat: created the socket cors options object
    Close: #8

--- apps/server/src/config/cors.ts
+++ apps/server/src/config/cors.ts
@@ -4,3 +4,8 @@ export const serverCorsOptions = {
   origin: config.origin,
   credentials: true, // ? access-control-allow-credentials: true
 };
+
+export const socketCorsOptions = {
+  origin: config.origin,
+  methods: ['POST', 'GET'],
+};
