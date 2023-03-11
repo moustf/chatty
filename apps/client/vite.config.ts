25d0fd0c55f8e4b759f78a4cf353b29dcbd7c42f
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-11 16:49:23 +0200

    feat: edited the production domain for vite app
    Close: #14

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "http://localhost:8080",
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.NODE_ENV === 'production'
+      ? 'https://134.122.73.111:8080'
+      : "http://localhost:8080",
+  });
+}
