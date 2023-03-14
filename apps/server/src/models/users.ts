1ace6094aa0b67865e57fb851505ca0895d92830
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-14 18:19:35 +0200

    feat: created the users model with in schema validation
    Close: #19

---
+++ apps/server/src/models/users.ts
@@ -0,0 +1,43 @@
+import mongoose from 'mongoose';
+
+const usersSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, 'Please enter your first name!'],
+    },
+    lastName: {
+      type: String,
+      required: [true, 'Please enter your last name!'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide an email!'],
+      validate: {
+        validator: function (v: string) {
+          return /^[a-zA-z0-9]?.*\@[a-zA-z0-9]{1,}\.[a-zA-Z]{1,}$/.test(v);
+        },
+        message: 'Please provide a valid email address!',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password!'],
+      validate: {
+        validator: function (v: string) {
+          return /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&])[a-zA-Z0-9!@#$%^&]{5,}$/.test(
+            v
+          );
+        },
+        message:
+          'Passwords should be at least five characters containing at least one upper case letter, on lower case letter, on number, and one symbol!',
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+usersSchema.index({ firstName: 1, lastName: 1 }, { unique: true });
+usersSchema.index({ email: 1 }, { unique: true });
+
+export const User = mongoose.model('User', usersSchema);
