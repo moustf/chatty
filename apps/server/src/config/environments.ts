ed907a577d58b694f67ad1df31687670d0516a6c
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-14 14:46:25 +0200

    feat: imported the database uris for different phases and exported them from envaironment variables
    Close: #18

--- apps/server/src/config/environments.ts
+++ apps/server/src/config/environments.ts
@@ -2,11 +2,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { NODE_ENV, PORT, ORIGIN, SECRET_KEY } = process.env;
+const {
+  NODE_ENV,
+  PORT,
+  ORIGIN,
+  SECRET_KEY,
+  PRODUCTION_DB_URI,
+  DEVELOPMENT_DB_URI,
+  TEST_DB_URI,
+} = process.env;
 
 export const config = {
   nodeEnv: NODE_ENV,
   port: PORT,
   origin: ORIGIN || 'http://localhost:3000',
   secretKey: SECRET_KEY,
+  prodURI: PRODUCTION_DB_URI,
+  devURI: DEVELOPMENT_DB_URI,
+  testURI: TEST_DB_URI,
 };
