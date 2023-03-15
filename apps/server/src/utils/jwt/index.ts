516d6f4e99c88bd0e9d759c7a82805260a6e6ef9
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-16 00:26:08 +0200

    feat: added the json web token generate and verify functions
    Close: #24

---
+++ apps/server/src/utils/jwt/index.ts
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken';
+
+import { config } from '../../config/environments';
+
+const { secretKey } = config;
+
+export const generateToken = async (payload: { email: string }) =>
+  new Promise((res, rej) => {
+    jwt.sign(payload, secretKey as string, (error, token) => {
+      if (error) rej(error);
+
+      res(token);
+    });
+  });
+
+export const verifyToken = async (token: string) =>
+  new Promise((res, rej) => {
+    jwt.verify(token, secretKey as string, (error, decoded) => {
+      if (error) rej(error);
+
+      res(decoded);
+    });
+  });
