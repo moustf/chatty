43341a92d43db3cb185cae7c60d13bdf9f50f0ea
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-17 23:13:00 +0200

    feat: separated the signup welcome section to its own component
    Close: #26

--- apps/client/src/pages/Signup.tsx
+++ apps/client/src/pages/Signup.tsx
@@ -1,27 +1,10 @@
 import { FC } from 'react';
 
-import Logo from '../assets/Logo.svg';
-import SideImage from '../assets/sl-side-image.svg';
+import { SignupWelcomeSection } from '../components/SingupWelcomeSection';
 
 export const SignupPage: FC = () => (
   <div className="w-full h-screen flex">
-    <section
-      className="w-1/3 h-screen shadow-z24 flex-col gap-60 hidden lg:flex"
-    >
-      <img
-        className="ml-12 mt-14"
-        src={Logo}
-        alt="logo"
-        width="64"
-        height="64"
-      />
-      <section className="w-full">
-        <h1
-          className="text-bold ml-12 mb-8 lg:text-h3 xl:text-h1"
-        >Welcome to Chatty!</h1>
-        <img src={SideImage} alt="side welcome" width="480" height="360" />
-      </section>
-    </section>
+    <SignupWelcomeSection />
     <main
       className="w-2/3 h-screen flex justify-center items-center"
     >
