ee9e356168a36fd9dabe4c8b1e8fffec6475fd17
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-21 14:44:41 +0200

    feat: created the auth functionality from the component
    Changed the file name and made it contains the whole component or section
    
    Close: #38

---
+++ apps/client/src/components/SocialMediaAuthSection.tsx
@@ -0,0 +1,33 @@
+import { FC  } from 'react';
+
+import GoogleIcon from '../assets/google-icon.svg';
+import FacebookIcon from '../assets/facebook-icon.svg';
+import TwitterIcon from '../assets/twitter-icon.svg';
+import { useAuthProvider } from '../hooks/useAuthProvider';
+
+type AuthButtonProps = {
+  icon: string;
+  provider: string;
+  provideAuth: (provider: string) => void;
+}
+
+const SocialMediaAuthButton: FC<AuthButtonProps> = ({ icon, provider, provideAuth }) => (
+  <button
+    className="w-1/3 h-14 border border-grey border-solid rounded-xl flex justify-center items-center hover:shadow-z12 hover:border-grey-200"
+    onClick={() => provideAuth(provider)}
+  >
+    <img src={icon} alt="social media icon" />
+  </button>
+);
+
+export const SocialMediaAuthSection: FC = () => {
+  const authProvider = useAuthProvider();
+
+  return (
+    <section className="w-full flex justify-between items-center gap-10 mb-8">
+      <SocialMediaAuthButton provider="google" provideAuth={authProvider} icon={GoogleIcon} />
+      <SocialMediaAuthButton provider="facebook" provideAuth={authProvider} icon={FacebookIcon} />
+      <SocialMediaAuthButton provider="twitter" provideAuth={authProvider} icon={TwitterIcon} />
+  </section>
+  );
+}
