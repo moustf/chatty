c65589717b019b361af91a1b79aa18ec7692ea8f
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-22 16:37:45 +0200

    fix: refactored the tests for the user data auth route
    Close: #39

--- apps/server/src/__tests__/userAuth.test.ts
+++ apps/server/src/__tests__/userAuth.test.ts
@@ -3,20 +3,23 @@ import request from 'supertest';
 import { app } from '../server';
 
 describe('Testing the user auth route for getting the user data', () => {
-  test('Testing the success case, returning the success status code the email fo the user', (done) => {
-    request(app)
+  test('Testing the success case, returning the success status code the email fo the user', async () => {
+    await request(app).post('/api/v1/auth/signup').send({
+      firstName: 'hima',
+      lastName: 'hims',
+      email: 'hima@gmail.com',
+      password: 'Root@123',
+    });
+
+    const res = await request(app)
       .post('/api/v1/auth')
       .set('Cookie', [
         'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhpbWFAZ21haWwuY29tIiwiaWF0IjoxNjc5NDY0NjQzfQ.rR-Ij2gT-9_q1mlLHbEwYPC3fGNLU3ONJ5o_WSnvha8',
       ])
-      .expect(200)
-      .end((error, res) => {
-        if (error) return done(error);
+      .expect(200);
 
-        expect(res.body.msg).toBe('The user is authenticated successfully!');
-        expect(res.body.data.email).toBe('hima@gmail.com');
-        done();
-      });
+    expect(res.body.msg).toBe('The user is authenticated successfully!');
+    expect(res.body.data.email).toBe('hima@gmail.com');
   });
 
   test('Testing the failure case, returning the unauthorized status code the unauthenticated message', (done) => {
