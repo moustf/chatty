9a50c255ddbec5188aa220a1cb3c830097bdc90e
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-25 18:19:20 +0300

    feat: created the login form and its functionality
    Close: #47

--- apps/client/src/pages/Login.tsx
+++ apps/client/src/pages/Login.tsx
@@ -1,14 +1,95 @@
 import { FC } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useForm, SubmitHandler, FieldValue } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { LoginData, validateLoginData } from '@chatty/types';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 import { SignupLoginWelcomeSection } from '../components/SignupLoginWelcomeSection';
+import { SocialMediaAuthSection } from '../components/SocialMediaAuthSection';
+import { OrSeparator } from '../components/OrSeparator';
+import { InputField } from '../components/InputField';
 
-export const LoginPage: FC = () => (
-  <div className="w-full h-screen flex">
+const baseUrl = import.meta.env.VITE_APP_BASE_URL;
+
+export const LoginPage: FC = () => {
+  const { mutate } = useMutation({
+    mutationFn: (data: LoginData) => (
+      axios.post(`${baseUrl}/api/v1/auth/login`, data)
+    ),
+    onError: (error: any) => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error?.response.data.msg + " " + error?.response.status,
+      });
+    },
+    onSuccess: () => {
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'The user has logged in successfully!',
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+  });
+  const { handleSubmit, control, formState: { errors, dirtyFields } } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: yupResolver(validateLoginData),
+  });
+
+  const isValid = dirtyFields.email && dirtyFields.password;
+
+  const onLoginSubmit: SubmitHandler<FieldValue<any>> = handleSubmit((data: LoginData) => mutate(data));
+
+  return (
+    <div className="w-full h-screen flex">
     <SignupLoginWelcomeSection text="Hi, Welcome Back!" />
     <main
       className="w-full h-screen flex justify-center lg:w-2/3"
     >
-      hello
+        <section
+          className="w-4/5 h-screen flex flex-col justify-center lg:w-2/3"
+        >
+          <section className="mb-10">
+            <h2 className="text-bold text-h3 md:text-h2 mb-8">Sign in to Chatty!</h2>
+            <p className="text-subtitle1 text-grey-600">
+              Don't you have an account?
+              <a className="text-[#2065D1] underline hover:text-info-darker visited:text-secondary" href="/sginup">Sign up</a>
+              {"  "}
+              .
+            </p>
+          </section>
+          <SocialMediaAuthSection />
+          <OrSeparator />
+          <form onSubmit={onLoginSubmit}>
+            <InputField
+              control={control}
+              fieldName="email"
+              placeholder="Please enter your email!"
+              type="email"
+            />
+            <InputField
+              control={control}
+              fieldName="password"
+              placeholder="Please enter your password!"
+              type="password"
+            />
+            <button
+              className={
+                `w-full h-14 mt-2 lg:mt-12 bg-secondary rounded-lg text-[#fff] text-btn font-bold cursor-pointer hover:shadow-secondary ${!isValid && 'cursor-not-allowed'}`
+              }
+              type="submit"
+              disabled={!isValid}
+            >Login</button>
+          </form>
+        </section>
     </main>
   </div>
-);
+  );
+};
--- packages/types/interfaces/index.ts
+++ packages/types/interfaces/index.ts
@@ -1,6 +1,7 @@
 export * from './Conversation';
 export * from './CustomSocketInterface';
 export * from './InputField';
+export * from './LoginData';
 export * from './messages';
 export * from './redux';
 export * from './User';
