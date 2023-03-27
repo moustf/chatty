5593f26d8177c0f718ad77836c10e5f1e61e03cf
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-27 11:01:40 +0300

    fix: fixed the tests for the user info route
    Close: #51

--- apps/server/src/__tests__/getUserById.ts
+++ apps/server/src/__tests__/getUserById.ts
@@ -4,25 +4,26 @@ import { app } from '../server';
 import { generateToken } from '../utils/jwt';
 
 describe('Testing the signup route', () => {
-  test('Testing the success case, the route should return the users info and the 200 status code', (done) => {
-    request(app)
-      .get('/api/v1/user')
-      .set('Cookie', [
-        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNTdmNzA4ZWQwYzU3MDU0MDA4ZTMwMCIsImVtYWlsIjoibXVzdGFmYUBnbWFpbC5jb20iLCJpYXQiOjE2Nzk4Nzg3MzR9.Gj-cDCv8kj2cSP0kWWYo_5eMMwMMDKzEC3bQGutRjNE',
-      ])
-      .expect(200)
-      .end((error, res) => {
-        if (error) return done(error);
+  test('Testing the success case, the route should return the users info and the 200 status code', async () => {
+    const user = await request(app).post('/api/v1/auth/signup').send({
+      firstName: 'test',
+      lastName: 'test',
+      email: 'test@gmail.com',
+      password: 'Test@123',
+    });
 
-        expect(res.body.data.email).toBe('mustafa@gmail.com');
-        return done();
-      });
+    const token = user.header['set-cookie'][0].split('=')[1].split(';')[0];
+
+    const res = await request(app)
+      .get('/api/v1/user')
+      .set('Cookie', [`token=${token}`])
+      .expect(200);
+
+    expect(res.body.data.email).toBe('test@gmail.com');
   });
 
   test('Testing the failure case, the route should return not found msg and the 404 status code', async () => {
     const token = await generateToken({ id: 93739, email: 'test@gmail.com' });
-
-    console.log(token, 'tokkkkkkkkkkkken');
 
     const res = await request(app)
       .get('/api/v1/user')
