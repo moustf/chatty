66a6230c0461877043b8c34c3efd15c58ba91512
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-27 17:50:31 +0300

    fix: fixed the source of the token for the tests
    Close: #52

--- apps/server/src/__tests__/searchForUsers.test.ts
+++ apps/server/src/__tests__/searchForUsers.test.ts
@@ -3,52 +3,59 @@ import request from 'supertest';
 import { app } from '../server';
 
 describe('Testing the route for searching for user', () => {
-  test('Testing the success case, the route should return the success status code and the user info', (done) => {
-    request(app)
+  test('Testing the success case, the route should return the success status code and the user info', async () => {
+    const user = await request(app).post('/api/v1/auth/signup').send({
+      firstName: 'Hadi',
+      lastName: 'Hadi',
+      email: 'hadi@gmail.com',
+      password: 'Test@123',
+    });
+
+    const token = user.headers['set-cookie'][0].split('=')[1].split(';')[0];
+
+    const res = await request(app)
       .get('/api/v1/user/find?q=mustafa')
-      .set('Cookie', [
-        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNTdmNzA4ZWQwYzU3MDU0MDA4ZTMwMCIsImVtYWlsIjoibXVzdGFmYUBnbWFpbC5jb20iLCJpYXQiOjE2Nzk4Nzg3MzR9.Gj-cDCv8kj2cSP0kWWYo_5eMMwMMDKzEC3bQGutRjNE',
-      ])
-      .expect(200)
-      .end((error, res) => {
-        if (error) return done(error);
+      .set('Cookie', [`token=${token}`])
+      .expect(200);
 
-        expect(res.body.data[0].email).toBe('mustafa@gmail.com');
-        return done();
-      });
+    expect(res.body.data[0].email).toBe('mustafa@gmail.com');
   });
 
-  test('Testing the failure case, the route should return the not found status code and the user does not exist message', (done) => {
-    request(app)
+  test('Testing the failure case, the route should return the not found status code and the user does not exist message', async () => {
+    const user = await request(app).post('/api/v1/auth/signup').send({
+      firstName: 'Hadi',
+      lastName: 'Hadi',
+      email: 'hadi@gmail.com',
+      password: 'Test@123',
+    });
+
+    const token = user.headers['set-cookie'][0].split('=')[1].split(';')[0];
+
+    const res = await request(app)
       .get('/api/v1/user/find?q=haraty')
-      .set('Cookie', [
-        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNTdmNzA4ZWQwYzU3MDU0MDA4ZTMwMCIsImVtYWlsIjoibXVzdGFmYUBnbWFpbC5jb20iLCJpYXQiOjE2Nzk4Nzg3MzR9.Gj-cDCv8kj2cSP0kWWYo_5eMMwMMDKzEC3bQGutRjNE',
-      ])
-      .expect(404)
-      .end((error, res) => {
-        if (error) return done(error);
+      .set('Cookie', [`token=${token}`])
+      .expect(404);
 
-        expect(res.body.msg).toBe(
-          'The user you are searching for does not exist!'
-        );
-        return done();
-      });
+    expect(res.body.msg).toBe('The user you are searching for does not exist!');
   });
 
-  test('Testing the failure case, the route should return the bad request status code and the wrong data message', (done) => {
-    request(app)
-      .get('/api/v1/user/find')
-      .set('Cookie', [
-        'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNTdmNzA4ZWQwYzU3MDU0MDA4ZTMwMCIsImVtYWlsIjoibXVzdGFmYUBnbWFpbC5jb20iLCJpYXQiOjE2Nzk4Nzg3MzR9.Gj-cDCv8kj2cSP0kWWYo_5eMMwMMDKzEC3bQGutRjNE',
-      ])
-      .expect(400)
-      .end((error, res) => {
-        if (error) return done(error);
+  test('Testing the failure case, the route should return the bad request status code and the wrong data message', async () => {
+    const user = await request(app).post('/api/v1/auth/signup').send({
+      firstName: 'Hadi',
+      lastName: 'Hadi',
+      email: 'hadi@gmail.com',
+      password: 'Test@123',
+    });
 
-        expect(res.body.msg).toBe(
-          'The text you are searching with does not belong to any user!'
-        );
-        return done();
-      });
+    const token = user.headers['set-cookie'][0].split('=')[1].split(';')[0];
+
+    const res = await request(app)
+      .get('/api/v1/user/find')
+      .set('Cookie', [`token=${token}`])
+      .expect(400);
+
+    expect(res.body.msg).toBe(
+      'The text you are searching with does not belong to any user!'
+    );
   });
 });
