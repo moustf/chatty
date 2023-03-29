13680153b6a9ada37ca505fc18b5248af5fe2d0f
Mustafa Salem <mustafaissasalem@gmail.com>
2023-03-29 12:50:24 +0300

    fix: fixed some style bug on the small screens
    Close: #54

--- apps/client/src/components/main-page/NotificationsMenu.tsx
+++ apps/client/src/components/main-page/NotificationsMenu.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import NotificationsIcon from '../../assets/Notifications.svg';
 
 export const NotificationsMenu: FC<{ type: string }> = ({ type }) => (
-  <section className={`block ${type === 'small' && 'lg:hidden'} w-10 h-10 mr-6 flex justify-content items-center`}>
+  <section className={`block ${type === 'small' && 'lg:hidden'} ${type === 'large' && 'sm:hidden lg:flex'} w-10 h-10 mr-6 flex justify-content items-center`}>
     <img
       className="box-border p-2 cursor-pointer hover:border hover:border-grey hover:border-solid hover:shadow-z24"
       src={NotificationsIcon}
