7a4cf558bbe49f760d764cb79af1271967eab472
Mustafa Salem <mustafaissasalem@gmail.com>
2023-04-19 18:23:43 +0300

    feat: created the user conversations list component and added it the the main page
    Close: #69

--- apps/client/src/pages/Main.tsx
+++ apps/client/src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import { FC } from 'react';
 import { MainHeader } from '../components/main-page/MainHeader';
+import { UserConversationList } from '../components/main-page/UserConversationsList';
 
 export const MainPage: FC = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="w-full h-screen flex flex-col justify-betwee">
       <MainHeader />
+      <UserConversationList />
     </div>
   );
 };
