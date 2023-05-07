afeb0810515d2249496a9fcea653318c8aa05122
Mustafa Salem <mustafaissasalem@gmail.com>
2023-05-07 23:11:01 +0300

    feat: added a type for the messages object and edited the payload of the redux store type
    Close: #86

---
+++ packages/types/interfaces/client/MessageObject.ts
@@ -0,0 +1,9 @@
+export type MessageObject = {
+  _id: string;
+  user: string;
+  type: string;
+  text: string;
+  name: string;
+  createdAt: string;
+  updatedAt: string;
+};
--- packages/types/interfaces/client/redux.ts
+++ packages/types/interfaces/client/redux.ts
@@ -1,3 +1,5 @@
+import { CustomMessagesHashTable } from '../../custom_data_types/CustomHashMap';
+
 export type Payload = {
   auth: {
     userData: { id: string; email: string; iat: number };
@@ -5,4 +7,5 @@ export type Payload = {
     error: string;
   };
   chat: { isChatsListShown: boolean };
+  messages: typeof CustomMessagesHashTable;
 };
