132503a034ce886188e884b26d3e08b34928331e
Mustafa Salem <mustafaissasalem@gmail.com>
2023-05-22 17:04:40 +0300

    fix: added some commits to track the base url in the socket setup

--- apps/client/src/hooks/useConnect.ts
+++ apps/client/src/hooks/useConnect.ts
@@ -3,6 +3,9 @@ import { io, Socket } from 'socket.io-client';
 import { SocketEventsMap } from '@chatty/types';
 import { baseUrl } from '../config/environment';
 
+console.log(import.meta.env.VITE_APP_BASE_URL, 'local env');
+console.log(import.meta.env.VITE_APP_PROD_BASE_URL, 'production env');
+
 const socket: Socket<SocketEventsMap> = io(`${baseUrl}/chat`, {
   transports: ['websocket'],
 }); // ? Keep it here to create only one socket instance.
