f61bb202ef665825b4968f7ceb6abe3173ff83f9
Mustafa Salem <mustafaissasalem@gmail.com>
2023-05-22 18:59:03 +0300

    fix: added more logs

--- apps/client/src/config/environment.ts
+++ apps/client/src/config/environment.ts
@@ -1,3 +1,6 @@
 export const baseUrl = import.meta.env.MODE === 'development'
   ? import.meta.env.VITE_APP_BASE_URL
   : import.meta.env.VITE_APP_PROD_BASE_URL;
+
+console.log(import.meta.env.MODE, 'current node environment!');
+console.log(import.meta.env.SSR, 'current SSR!');
--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+console.log(process.cwd(), 'current working directory');
+
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
