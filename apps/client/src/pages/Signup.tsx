2e18a814ee6fab885c9074cf3b9ba0feaf55b6fb
Mustafa Salem <mustafaissasalem@gmail.com>
2023-05-27 13:53:11 +0300

    refactor: divided the signup process into two steps and added an eye to see the password text
    Refactored the error messages functionality and edited some styles
    
    Close: #89

--- apps/client/src/pages/Signup.tsx
+++ apps/client/src/pages/Signup.tsx
@@ -1,9 +1,9 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { useMutation } from '@tanstack/react-query';
 import { useForm, SubmitHandler, FieldValue } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { User } from '@chatty/types';
-import { userDataSchema } from '@chatty/types';
+import { userDataComponentSchema } from '@chatty/types';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
@@ -12,9 +12,29 @@ import { SocialMediaAuthSection } from '../components/SocialMediaAuthSection';
 import { OrSeparator } from '../components/OrSeparator';
 import { InputField } from '../components/InputField';
 import { baseUrl } from '../config/environment';
+import { useNavigate } from 'react-router-dom';
 
 export const SignupPage: FC = () => {
   axios.defaults.withCredentials = true;
+
+  const [step, setStep] = useState<number>(1);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const resetErrorMessage = () => setErrorMessage('');
+
+    const inputFields = document.querySelectorAll('input');
+    inputFields.forEach((input) => {
+      input.addEventListener('input', resetErrorMessage);
+    });
+
+    return () => {
+      inputFields.forEach((input) => {
+        input.removeEventListener('input', resetErrorMessage);
+      });
+    };
+  }, []);
 
   const { mutate } = useMutation({
     mutationFn: (user: User) => (
@@ -41,10 +61,15 @@ export const SignupPage: FC = () => {
         showConfirmButton: false,
         timer: 1500
       });
+
+      setTimeout(() => {
+        navigate('/login');
+      }, 1500);
     },
   });
+
   const {
-    handleSubmit, control, formState: { errors, dirtyFields }
+    handleSubmit, control, reset, formState: { errors, isValid }
   } = useForm({
     defaultValues: {
       firstName: '',
@@ -53,33 +78,25 @@ export const SignupPage: FC = () => {
       password: '',
       confirmPassword: '',
     },
-    resolver: yupResolver(userDataSchema),
+    resolver: yupResolver(userDataComponentSchema),
   });
-
-  const isValid = (
-    dirtyFields.firstName
-    && dirtyFields.lastName
-    && dirtyFields.email
-    && dirtyFields.password
-    && dirtyFields.confirmPassword
-  );
 
   const onSignupSubmit: SubmitHandler<FieldValue<any>> = handleSubmit((data: User) => mutate(data));
 
-  if (Object.keys(errors).length) {
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: errors.firstName
+  if (Object.keys(errors).length && !errorMessage) {
+    setErrorMessage(
+      errors.firstName
         ? 'First Name is required!'
         : errors.lastName
           ? 'Last Name is required!'
           : errors.email
             ? 'Please provide a valid email!'
             : errors.password
-              ? 'Password should contain one upper letter, one lower letter, on number, on symbol, and at least 5 characters!'
-              : 'The two passwords should match!',
-    });
+              ? 'Password should contain one upper letter, one lower letter, one number, one symbol, and at least 5 characters!'
+              : errors.confirmPassword
+                ? 'The two passwords should match!'
+                : 'Unknown error!'
+    );
   }
 
   return (
@@ -102,31 +119,63 @@ export const SignupPage: FC = () => {
           </section>
           <SocialMediaAuthSection />
           <OrSeparator />
+          {errorMessage && (
+            <div className="text-error text-[0.8rem] my-2">{errorMessage}</div>
+          )}
           <form onSubmit={onSignupSubmit}>
-            {
-              [
-                { name: 'firstName', type: 'text', placeholder: 'First Name' },
-                { name: 'lastName', type: 'text', placeholder: 'Last Name' },
-                { name: 'email', type: 'email', placeholder: 'Email' },
-                { name: 'password', type: 'password', placeholder: 'Password' },
-                { name: 'confirmPassword', type: 'password', placeholder: 'Confirm Password' },
-              ].map((obj, ind) => (
-                <InputField
-                  key={ind}
-                  control={control}
-                  fieldName={obj.name}
-                  type={obj.type}
-                  placeholder={obj.placeholder}
-                />
-              ))
-            }
+            <section className={`${step === 1 ? 'flex' : 'hidden'} flex-col`}>
+              {
+                [
+                  { name: 'firstName', type: 'text', placeholder: 'First Name' },
+                  { name: 'lastName', type: 'text', placeholder: 'Last Name' },
+                  { name: 'email', type: 'email', placeholder: 'Email' },
+                ].map((obj, ind) => (
+                  <InputField
+                    key={ind}
+                    control={control}
+                    fieldName={obj.name}
+                    type={obj.type}
+                    placeholder={obj.placeholder}
+                  />
+                ))
+              }
+            </section>
+            <section className={`${step === 2 || step === 3 ? 'flex' : 'hidden'} flex-col`}>
+              {
+                [
+                  { name: 'password', type: 'password', placeholder: 'Password' },
+                  { name: 'confirmPassword', type: 'password', placeholder: 'Confirm Password' },
+                ].map((obj, ind) => (
+                  <InputField
+                    key={ind}
+                    control={control}
+                    fieldName={obj.name}
+                    type={obj.type}
+                    placeholder={obj.placeholder}
+                  />
+                ))
+              }
+            </section>
             <button
               className={
-                `w-full h-14 mt-2 lg:mt-12 bg-secondary rounded-lg text-[#fff] text-btn font-bold cursor-pointer hover:shadow-secondary ${!isValid && 'cursor-not-allowed'}`
+                `w-full h-12 mt-2 lg:mt-8 bg-secondary rounded-lg text-[#fff] text-btn font-bold cursor-pointer hover:shadow-secondary ${!isValid && 'cursor-not-allowed'}`
               }
-              type="submit"
-              disabled={!isValid}
-            >Sign Up</button>
+              type={step === 1 || step === 2 ? 'button' : 'submit'}
+              onClick={
+                step === 1 && !isValid
+                  ? () => setStep(2)
+                  : step === 2 && !isValid
+                    ? () => setStep(1)
+                    : () => setStep(3)
+              }
+              disabled={step === 3 && !isValid}
+            >{
+                step === 1 && !isValid
+                  ? 'Next'
+                  : step === 2 && !isValid
+                    ? 'Back'
+                    : 'Submit'
+              }</button>
           </form>
         </section>
       </main>
